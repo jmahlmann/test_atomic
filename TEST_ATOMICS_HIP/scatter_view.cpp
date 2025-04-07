2bbef4b5777e912769adab06e4e8c92522ab7aeb
jmahlmann <jens.mahlmann@gmail.com>
2025-04-07 19:02:37 -0400

    Ongoing

--- TEST_ATOMICS_HIP/scatter_view.cpp
+++ TEST_ATOMICS_HIP/scatter_view.cpp
@@ -1,36 +1,39 @@
 #include <hip/hip_runtime.h>
-#include <chrono>
 #include <iostream>
+#include <chrono>
 #include <cstdint>
 
 #define N 1000000
 
-__global__ void atomicAdd_int(int* counter) {
+__global__ void atomicAdd_int_kernel(int* counter) {
     atomicAdd(counter, 1);
 }
 
-__global__ void atomicAdd_int32(std::int32_t* counter) {
+__global__ void atomicAdd_int32_kernel(int32_t* counter) {
     atomicAdd(counter, 1);
 }
 
-__global__ void atomicAdd_int64(std::int64_t* counter) {
-    atomicAdd((unsigned long long int*)counter, 1ULL); // HIP workaround
+__global__ void atomicAdd_int64_kernel(int64_t* counter) {
+    atomicAdd((unsigned long long int*)counter, 1ULL);  // HIP requires casting
 }
 
-__global__ void atomicAdd_long(long int* counter) {
-    atomicAdd((unsigned long long int*)counter, 1ULL); // HIP workaround
+__global__ void atomicAdd_long_kernel(long int* counter) {
+    atomicAdd((unsigned long long int*)counter, 1ULL);
 }
 
-__global__ void atomicAdd_ulonglong(unsigned long long int* counter) {
+__global__ void atomicAdd_ulonglong_kernel(unsigned long long int* counter) {
     atomicAdd(counter, 1ULL);
 }
 
-__global__ void atomicAdd_double(double* counter) {
+__global__ void atomicAdd_double_kernel(double* counter) {
     atomicAdd(counter, 1.0);
 }
 
-template <typename T, void (*Kernel)(T*)>
-double run_atomic_test(const char* label) {
+template <typename T>
+using KernelFunc = void (*)(T*);
+
+template <typename T, KernelFunc<T> kernel>
+double run_atomic(const char* label) {
     T* d_counter;
     hipMalloc(&d_counter, sizeof(T));
     hipMemset(d_counter, 0, sizeof(T));
@@ -38,8 +41,9 @@ double run_atomic_test(const char* label) {
     auto start = std::chrono::high_resolution_clock::now();
 
     for (int i = 0; i < N; ++i) {
-        Kernel<<<1,1>>>(d_counter);
+        kernel<<<1, 1>>>(d_counter);
     }
+
     hipDeviceSynchronize();
 
     auto end = std::chrono::high_resolution_clock::now();
@@ -52,12 +56,12 @@ double run_atomic_test(const char* label) {
 }
 
 int main() {
-    run_atomic_test<int, atomicAdd_int>("Atomic Int");
-    run_atomic_test<std::int32_t, atomicAdd_int32>("Atomic Int32");
-    run_atomic_test<std::int64_t, atomicAdd_int64>("Atomic Int64");
-    run_atomic_test<long int, atomicAdd_long>("Atomic LongInt");
-    run_atomic_test<unsigned long long int, atomicAdd_ulonglong>("Atomic LongLongInt");
-    run_atomic_test<double, atomicAdd_double>("Atomic Double");
+    run_atomic<int, atomicAdd_int_kernel>("Atomic Int");
+    run_atomic<int32_t, atomicAdd_int32_kernel>("Atomic Int32");
+    run_atomic<int64_t, atomicAdd_int64_kernel>("Atomic Int64");
+    run_atomic<long int, atomicAdd_long_kernel>("Atomic LongInt");
+    run_atomic<unsigned long long int, atomicAdd_ulonglong_kernel>("Atomic LongLongInt");
+    run_atomic<double, atomicAdd_double_kernel>("Atomic Double");
 
     return 0;
 }
